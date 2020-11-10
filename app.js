5e8448f7db622f2bd09820128b23fdbf705d5069
Ricardo <rikardo_kz@live.com.pt>
2020-11-10 18:14:24 +0000

    add html and styling

---
+++ app.js
@@ -0,0 +1,17 @@
+const img = document.getElementById("img");
+const name = document.getElementById("name");
+const stars = document.getElementById("stars");
+const text = document.getElementById("text");
+
+const opinions = {
+    feedbackOne: {
+        name: "Megatron",
+        stars: "⭐⭐⭐⭐",
+        opinion: "Wonderfull website, such WOW"
+    },
+    feedbackTwo: {
+        name: "Alien",
+        stars: "⭐⭐⭐",
+        opinion: "Basic stuff, but still better than Predator"
+    }
+}
